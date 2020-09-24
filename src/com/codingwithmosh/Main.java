1c42100af3d2747c57502859b685ad2f969434e5
Stephmarsjs <54284220+Stephmarsjs@users.noreply.github.com>
2020-09-23 23:16:43 -0400

    initial commit

---
+++ src/com/codingwithmosh/Main.java
@@ -0,0 +1,19 @@
+package com.codingwithmosh;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Number: ");
+        int number = scanner.nextInt();
+        if (number % 5 == 0)
+            System.out.println("Fizz");
+
+
+
+
+
+    }
+}
